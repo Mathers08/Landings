5462f6689597c1f9f8bca215ed4edbd53918e96d
Mathers08 <70015560+Mathers08@users.noreply.github.com>
2022-07-18 21:44:23 +0300

    create header and make it responsive

--- Pravt/js/script.js
+++ Pravt/js/script.js
@@ -1,25 +1,28 @@
-// Custom Scripts
 // Burger Menu
 function burgerMenu() {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
+  const info = document.querySelector('.info');
   const body = document.querySelector('.body');
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
       burger.classList.add('active-burger');
       menu.classList.add('active');
+      info.classList.add('active');
       body.classList.add('locked');
     } else {
       burger.classList.remove('active-burger');
       menu.classList.remove('active');
+      info.classList.remove('active');
       body.classList.remove('locked');
     }
   })
   // Вот тут мы ставим breakpoint навбара
   window.addEventListener('resize', () => {
-    if (window.innerWidth > 911.98) {
+    if (window.innerWidth > 995) {
       burger.classList.remove('active-burger');
       menu.classList.remove('active');
+      info.classList.remove('active');
       body.classList.remove('locked');
     }
   })
