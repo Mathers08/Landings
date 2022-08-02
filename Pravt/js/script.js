9f028ee277e6ce1f4567ad424629e5179dae4c8f
Mathers08 <70015560+Mathers08@users.noreply.github.com>
2022-08-02 15:43:02 +0300

    fix burger menu

--- Pravt/js/script.js
+++ Pravt/js/script.js
@@ -1,28 +1,45 @@
-// Custom Scripts
-
 // Burger Menu
 function burgerMenu() {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
-  const body = document.querySelector('.body');
+  const info = document.querySelector('.info');
+  const body = document.querySelector('.main__body');
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
       burger.classList.add('active-burger');
       menu.classList.add('active');
+      info.classList.add('active');
       body.classList.add('locked');
     } else {
       burger.classList.remove('active-burger');
       menu.classList.remove('active');
+      info.classList.remove('active');
       body.classList.remove('locked');
     }
   })
   // Вот тут мы ставим breakpoint навбара
   window.addEventListener('resize', () => {
-    if (window.innerWidth > 911.98) {
+    if (window.innerWidth > 995) {
       burger.classList.remove('active-burger');
       menu.classList.remove('active');
+      info.classList.remove('active');
       body.classList.remove('locked');
     }
   })
 }
 burgerMenu();
+
+// Footer
+function textChange() {
+  const footerLeftText = document.querySelector('.footer__left-text');
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 740) {
+      footerLeftText.textContent = 'Производство запасных частей \n' +
+        'для грузового коммерческого\n' +
+        'транспорта';
+    } else {
+      footerLeftText.textContent = 'Продажа запасных частей ГАЗ, ВАЗ, ПАЗ, УАЗ, КАМАЗ';
+    }
+  })
+}
+textChange();
